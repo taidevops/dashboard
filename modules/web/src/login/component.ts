35ace385d79d7b75dd446f81d73cdca7560b1818
Phan Tấn Tài <37982283+4201104140@users.noreply.github.com>
2022-09-26 17:25:39 +0000

    tai

--- modules/web/src/login/component.ts
+++ modules/web/src/login/component.ts
@@ -7,12 +7,23 @@ enum LoginModes {
 }
 
 export class LoginComponent implements OnInit {
+  loginModes = LoginModes;
+
+  errors: KdError[] = [];
+
+  private enablesAuthenticationModes_: AuthenticationMode[] = [];
 
   constructor(
-
+    private readonly http_: HttpClient,
   ) {}
 
   ngOnInit(): void {
-    
+    this.http_
+      .get<EnabledAuthenticationModes>('api/v1/login/modes')
+      .subscribe((enabledMode: EnableAuthenticationModes) => {
+        this.enablesAuthenticationModes_ = enabledMode.modes;
+        this.enablesAuthenticationModes_.push(LoginModes.Kubeconfig);
+        this.selectedAuthenticationMode as LoginModels)
+      })
   }
 }
---
+++ modules/web/src/login/module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+
+import {ComponentsModule} from '@common/components/module';
+import {SharedModule} from '../shared.module';
+import {LoginComponent} from './component';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [SharedModule, ComponentsModule],
+})
+export class LoginModule {}
